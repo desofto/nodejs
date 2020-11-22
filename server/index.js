9525cba9dbc0849f780d9c090f0b86168e1f18d5
Dmitry Silchenko <dmitry@desofto.com>
2020-11-22 13:18:18 +0100

    Introduce express server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,39 @@
+import express from "express"
+import "babel-polyfill"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static(__dirname + "/../public"))
+
+app.get("/apiql", async (req, res) => {
+  res.status(200).sendFile("apiql.html", { root: __dirname + "/../public" })
+})
+
+app.post("/apiql", async (req, res) => {
+  const { token } = req.headers
+  if (!token) {
+    return res.status(404).send("Not Found")
+  }
+  res.status(200).send("OK")
+})
+
+app.listen(process.env.PORT).on("listening", () => {
+  console.log(`Started on port ${process.env.PORT}`)
+})
+
+/*
 import pool from "../shared/pool"
 import User from "../models/user"
 
 (async () => {
+
   let users = await User.where("email ilike $1", ["qwe"])
   console.log(users.length)
 
@@ -17,3 +49,4 @@ import User from "../models/user"
 
   pool.end()
 })()
+*/
